24715f3f1fffc9730113a68194e5480ed5873046
TXXXX-ux <mambetovkanat7@gmail.com>
2026-04-15 17:30:35 +0600

    add server starter

---
+++ src/main/java/kg/attractor/lesson55lab/Lesson57labApplication.java
@@ -0,0 +1,13 @@
+package kg.attractor.lesson55lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson57labApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson57labApplication.class, args);
+	}
+
+}
