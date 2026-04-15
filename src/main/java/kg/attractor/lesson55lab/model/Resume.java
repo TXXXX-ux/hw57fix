a14cd42a2178ce98f0897646ddb09527eb4fb835
TXXXX-ux <mambetovkanat7@gmail.com>
2026-04-15 17:46:39 +0600

    add method "description"

--- src/main/java/kg/attractor/lesson55lab/model/Resume.java
+++ src/main/java/kg/attractor/lesson55lab/model/Resume.java
@@ -1,16 +1,14 @@
 package kg.attractor.lesson55lab.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Resume {
     private Long id;
     private String title;
-    private String info;
+    private String description;
 }
